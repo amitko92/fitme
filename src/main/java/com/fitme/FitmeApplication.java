2e0bfc200f8031e749ea86524bc699e1a8f34402
amitko92 <amitko92@gmail.com>
2025-08-30 16:59:15 +0300

    init commit

---
+++ src/main/java/com/fitme/FitmeApplication.java
@@ -0,0 +1,13 @@
+package com.fitme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmeApplication.class, args);
+	}
+
+}
